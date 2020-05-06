03856aeae380e5d0cd9eddf2b8f59dd8fccb1c52
RichardDWoodhead <Richardwoodhead00@gmail.com>
2020-05-06 12:36:06 +0000

    added apps
    added apps for home and user and more for the index template

--- main/store/views.py
+++ main/store/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 products = [
-    {"name": "ps2", "price":15000},
-    {"name": "ps3", "price":20000},
-    {"name": "ps4", "price":35000},
+    {"name": "ps2", "price":15000, "image": "images/ps2.png"},
+    {"name": "ps3", "price":20000, "image": "images/ps3.jpg"},
+    {"name": "ps4", "price":35000, "image": "images/ps4.jpg"},
 ]
 
 
