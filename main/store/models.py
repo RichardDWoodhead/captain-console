54425d33e93f5f920eb27f3e4c575413886a001a
RichardDWoodhead <Richardwoodhead00@gmail.com>
2020-05-06 18:27:46 +0000

    Update models.py

--- main/store/models.py
+++ main/store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    manufacturer = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.BooleanField()
