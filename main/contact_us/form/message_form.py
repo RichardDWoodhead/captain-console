a86b84823ea969097f6799ed11d4b192a4c51c04
Birna Valtýsdóttir <birnavaltys@gmail.com>
2020-05-07 15:22:32 +0000

    models for user & store + contact_us app
    bættum við models i user og store
    og contact_us app.

---
+++ main/contact_us/form/message_form.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm, widgets
+
+from contact_us.models import Messages
+
+class ContactUs(ModelForm):
+    class Meta:
+        model = Messages
+        exclude = ['id']
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
+            'email': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+            'phonenumber': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'})
+            'message': widgets.Textarea(attrs={'class': 'form-control', 'id': 'comment', 'placeholder': 'Your Message'})
+
+        }
