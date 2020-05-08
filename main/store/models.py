fa57807967bf2e60afad6dc75ab74434f3aa1f74
Birna Valtýsdóttir <birnavaltys@gmail.com>
2020-05-08 11:44:56 +0000

    models klarad

--- main/store/models.py
+++ main/store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -11,6 +12,7 @@ class Product(models.Model):
     def __str__(self):
         return self.status
 
+
 class ProductImage(models.Model):
     image = models.CharField(max_length=999)
     mainimage = models.BooleanField()
@@ -18,11 +20,25 @@ class ProductImage(models.Model):
     def __str__(self):
         return self.image
 
-class ProductOrder(models.Model):
-    cardholder_name = models.CharField(max_length=255)
-    cardnumber = models.CharField(max_length=255)
-    exp = models.IntegerField()
-    cvv = models.CharField(max_length=5)
+
+class Order(models.Models):
     date = models.DateField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cardholder_name = models.CharField(max_length=255)
+    card_number = models.CharField(max_length=255)
+
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    def __str__(self):
+        return self.product
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    def __str__(self):
+        return self.product
--- main/user/models.py
+++ main/user/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 # Create your models here.
+
 
 class User(models.Model):
     first_name = models.CharField(max_length=255)
