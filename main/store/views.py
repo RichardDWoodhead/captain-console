d0095d28eb4e6e9d18b1ae923c1e04c88f3db1d7
RichardDWoodhead <Richardwoodhead00@gmail.com>
2020-05-10 16:49:24 +0000

    added carousel to product details

--- main/store/views.py
+++ main/store/views.py
@@ -16,18 +16,27 @@ def index(request):
 
 
 def get_products(request):
-    products = [ {
-        'id' : x["id"],
-        'name' : x["name"],
-        'description' : x["description"],
+    products = [{
+        'id': x["id"],
+        'name': x["name"],
+        'description': x["description"],
         'price': x["price"],
-        'image' : str(ProductImage.objects.raw("SELECT id from store_productimage WHERE product_id = 1 AND mainimage")[0].image),
+        'image': str(ProductImage.objects.raw("SELECT id from store_productimage WHERE product_id = "+str(x["id"])+" AND mainimage")[0].image),
     } for x in list(Product.objects.values())]
     return JsonResponse({"products": products})
 
 
 def product(request, product_id):
-    return render(request, "store/product_details.html", context={"product": products[product_id]})
+    cproduct = [{
+        'id': x.id,
+        'name': x.name,
+        'description': x.description,
+        'price': x.price,
+        'other_images': list(ProductImage.objects.raw("SELECT id from store_productimage WHERE product_id ="+str(product_id)+" AND NOT mainimage")),
+        'main_image': str(ProductImage.objects.raw("SELECT id from store_productimage WHERE product_id = "+str(x.id)+" AND mainimage")[0].image),
+    } for x in list(Product.objects.raw("SELECT id from store_product WHERE id ="+str(product_id)))]
+    print(cproduct[0]["other_images"])
+    return render(request, "store/product_details.html", context={"product": cproduct[0]})
 
 
 def payment(request):
