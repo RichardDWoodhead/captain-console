eca13f0a97e61f6d490081ad9cfed5277251781d
Arnarps <54935919+Arnarps@users.noreply.github.com>
2020-05-11 14:37:39 +0000

    login/register
    added templates for login/register

--- main/user/views.py
+++ main/user/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def user(request):
     return render(request, "user/index.html")
+def login(request):
+    return render(request, "user/login.html")
+def register(request):
+    return render(request, "user/register.html")
